ebe9df25a8976572d585337cb6b4bf343781e577
Ashen Bamunusingha <ashen0217@gmail.com>
2025-04-27 15:16:57 +0530

    asa
    asaa

---
+++ backend/Model/payModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const paySchema = new Schema({
+    userName:{
+        type:String,
+        required:true
+    },
+    bank:{
+        type:Number,
+        required:true
+    },
+    branch:{
+        type:String,
+        required:true
+    },
+    package:{
+        type:String,
+        required:true
+    },
+    amount:{
+        type:Number,
+        required:true
+    },
+    slip:{
+        data:Buffer,  //binary data
+        contentType:String
+    },
+    cnfStatus:{
+        type:String,
+        required:true
+    }
+
+})
+
+module.exports = mongoose.model("payModel", paySchema);
