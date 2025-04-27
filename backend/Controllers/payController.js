81f7891228275fc7083e4d82c66884cd7c00eaa5
Ashen Bamunusingha <ashen0217@gmail.com>
2025-04-27 15:21:41 +0530

    asa
    asas

--- backend/Controllers/payController.js
+++ backend/Controllers/payController.js
@@ -1,2 +1,21 @@
-const Resource = require("../Model/payModel.js");
+const Payment = require("../Model/payModel.js");
 
+
+//data display
+const getAllResources = async (req, res, next) => {
+    let Payments;
+
+    try{
+        Payments = await Payment.find();
+    }catch(err){
+        console.log(err);
+    }
+
+    //not found
+    if(!Payments){
+        return res.status(404).json({message:"Payments not found"});
+    }
+
+    //display the resources
+    return res.status(200).json({ message:"Payments display successful",Payments});
+};
