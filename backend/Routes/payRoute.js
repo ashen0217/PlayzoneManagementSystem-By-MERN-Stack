82bc87dfa2b6a88176ebcb138ac86b2986814c83
Ashen <ashen0217@gmail.com>
2026-01-08 17:41:31 +0530

    fixed

--- backend/Routes/payRoute.js
+++ backend/Routes/payRoute.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const upload = multer(); // For memory storage, or configure as needed
+const formidable = require("formidable");
 
 //insert model
 const Payments = require("../Model/payModel");
